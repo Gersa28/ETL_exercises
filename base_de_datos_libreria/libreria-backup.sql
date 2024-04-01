5023200d607638221d40886edb3ffa574a9033d3
Germán Salina <gersalina28@gmail.com>
2024-04-01 18:34:02 -0300

    BBDD selection

--- base_de_datos_libreria/libreria-backup.sql
+++ base_de_datos_libreria/libreria-backup.sql
@@ -1,3 +1,5 @@
+/*Esto lo ejecutamos en el editor correspondiente al gestor de base de datos elegido*/
+
 CREATE DATABASE  IF NOT EXISTS `libreria` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `libreria`;
 -- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
